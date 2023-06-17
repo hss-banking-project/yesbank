6efc62d5d8fe0027eedf89509d01949580e3a2c6
devopshari00 <t.harikrishna15@gmail.com>
2023-06-17 23:10:32 +0530

    updated

--- stringutils.java
+++ stringutils.java
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 This string file contains the string related information
 ~
 =======
 This string file contains string related information
 >>>>>>> parent of ab95905 (Reverted Revert "added string information")
+=======
+This file contains string related information
+>>>>>>> parent of 423b0a3 (updated)
